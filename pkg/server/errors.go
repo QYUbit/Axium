edeb658424d7b9a9b854eba85ead908f54e971c5
QYUb <145557846+QYUbit@users.noreply.github.com>
2026-01-11 16:41:14 +0100

    remove dev packages

--- pkg/server/errors.go
+++
@@ -1,9 +0,0 @@
-package server
-
-type ErrAlreadyRunning struct{}
-
-func (ErrAlreadyRunning) Error() string { return "server is already running" }
-
-type ErrNotRunning struct{}
-
-func (ErrNotRunning) Error() string { return "server is not running" }
